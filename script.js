817c42260ced2b8a91ddc3cebd091193ab68d6c8
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2025-11-22 12:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 0;
+
+function addItems(num) {
+	for (let i = 0; i < num; i++) {
+		count++;
+		let li = document.createElement("li");
+		li.textContent = "item " + count;
+		list.appendChild(li);
+	}
+}
+
+	addItems(10);
+
+	list.addEventListener("scroll", () => {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+        addItems(2);
+    }
+});
